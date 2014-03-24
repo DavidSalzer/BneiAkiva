84949748a0156675660406c1aa5fc1cafd1b960e
Saronet <sarahillel436@gmail.com>
2014-03-24 15:12:06 +0200

    calender

--- wp-content/themes/DeepFocus/calender-page.php
+++ wp-content/themes/DeepFocus/calender-page.php
@@ -22,16 +22,14 @@ Template Name: לוח שנה
 	</div> <!-- end #hr -->
 
 	<div class="center-highlight">
-		<div class="container">
+		<div id="container-calender" class="container">
 
-			<div id="content-area" class="clearfix">
+			<div id="full" class="clearfix">
 
-				<div id="left-area">
 
 					<?php if (get_option('deepfocus_integration_single_top') <> '' && get_option('deepfocus_integrate_singletop_enable') == 'on') echo(get_option('deepfocus_integration_single_top')); ?>
-
 					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-						<div class="entry clearfix post">
+						<div class="entry clearfix post full">
 							<?php $width = 185;
 								  $height = 185;
 								  $classtext = '';
@@ -40,16 +38,25 @@ Template Name: לוח שנה
 								  $thumbnail = get_thumbnail($width,$height,$classtext,$titletext,$titletext);
 								  $thumb = $thumbnail["thumb"]; ?>
 
-							<h1 class="title"><?php the_title(); ?></h1>
+							<h1 class="title about-title"><?php the_title(); ?></h1>
+                            <!--<div id="embedded-calender">-->
+                                <div id="container-aboutpage" class="container">
+			                        <div id="full" class="clearfix about-clearfix">
+                                        <iframe src="https://www.google.com/calendar/embed?src=vfot1h1682mg47q2rido0vgeos%40group.calendar.google.com&ctz=Asia/Jerusalem" style="border: 0" width="540" height="400" frameborder="0" scrolling="no"></iframe>
+                                    </div>
+                                </div>
+                            <!--</div>-->
+                            <div id="events-calender"></div>
+							
 							<?php get_template_part('includes/postinfo'); ?>
 
 							<?php if($thumb == '') echo('<div class="clear"></div>'); ?>
 
 							<?php if($thumb <> '' && get_option('deepfocus_page_thumbnails') == 'on') { ?>
-									<div class="blog-thumb">
-										<?php print_thumbnail($thumb, $thumbnail["use_timthumb"], $titletext, $width, $height, $classtext); ?>
-										<span class="overlay"></span>
-									</div> <!-- end .blog-thumb -->
+								<div class="post-thumbnail">
+									<?php print_thumbnail($thumb, $thumbnail["use_timthumb"], $titletext , $width, $height, $classtext); ?>
+									<span class="overlay"></span>
+								</div> 	<!-- end .thumbnail -->
 							<?php }; ?>
 
 							<?php the_content(); ?>
@@ -57,17 +64,17 @@ Template Name: לוח שנה
 							<?php edit_post_link(esc_html__('Edit this page','DeepFocus')); ?>
 						</div> <!-- end .entry -->
 					<?php endwhile; endif; ?>
-
 					<?php if (get_option('deepfocus_integration_single_bottom') <> '' && get_option('deepfocus_integrate_singlebottom_enable') == 'on') echo(get_option('deepfocus_integration_single_bottom')); ?>
 
 					<?php if (get_option('deepfocus_show_pagescomments') == 'on') comments_template('', true); ?>
-
-				</div> <!-- end #left-area -->
-
-				<?php get_sidebar(); ?>
 
 			</div> <!-- end #content-area -->
 
 		</div> <!-- end .container -->
 
 		<?php get_footer(); ?>
+
+
+
+
+
